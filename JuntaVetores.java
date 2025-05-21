831d801302757bb198edd65440f3a54a234e3ae2
MariaMomberg <malumomberg2307@gmail.com>
2025-05-21 12:28:17 -0300

    Update JuntaVetores.java

--- JuntaVetores.java
+++ JuntaVetores.java
@@ -6,19 +6,19 @@ public class JuntaVetores {
 
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
-		int[] Vet1 = new int[2], Vet2 = new int [2], Vet3 = new int[4];
+		int[] num1 = new int[2], num2 = new int [2], num3 = new int[4];
 		for (int i = 0; i < 2; i++) {
 		System.out.println("Digite o " + (i + 1) + "º valor para 1:");
-		Vet1[i] = ler.nextInt(); }
+		num1[i] = ler.nextInt(); }
 		for (int i = 0; i < 2; i++) {
 		System.out.println("Digite o " + (i + 1) + "º valor para 2:");
-		Vet2[i] = ler.nextInt();}
+		num2[i] = ler.nextInt();}
 		for (int i = 0; i < 2; i++) {
-		Vet3[i] = Vet1[i];
-		Vet3[i + 2] = Vet2[i]; }
+		num3[i] = num1[i];
+                num3[i + 2] = num2[i]; }
 		System.out.println("Valores do vetor 3:");
 		for (int i = 0; i < 4; i++) {
-		System.out.println(Vet3[i]); }
+		System.out.println(num3[i]); }
 		ler.close();
 		}	
 	}
